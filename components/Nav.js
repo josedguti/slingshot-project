252e48ac0cea526389f80ef1b2c4511ba2f86ba4
Jose Gutierrez <78231620+josedguti@users.noreply.github.com>
2022-04-07 18:57:02 +0200

    added google auth

--- components/Nav.js
+++ components/Nav.js
@@ -131,7 +131,7 @@ function Nav() {
                 {!session ? (
                   <div className="flex-shrink-0">
                     <button
-                      onClick={() => signIn('github')}
+                      onClick={() => signIn()}
                       className="relative inline-flex items-center px-4 py-2 border border-transparent text-sm font-medium rounded-md text-white bg-green-600 shadow-sm hover:bg-green-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-green-500 ml-7"
                     >
                       Sign In
--- pages/index.js
+++ pages/index.js
@@ -89,7 +89,7 @@ export default function index() {
                     Sign In With Github
                   </p>
                   <button
-                    onClick={() => signIn("github")}
+                    onClick={() => signIn()}
                     className="w-full inline-flex justify-center py-2 px-4 border border-gray-300 rounded-md shadow-sm bg-white text-sm font-medium text-gray-500 hover:bg-gray-50"
                   >
                     <span className="sr-only">Sign in with GitHub</span>
