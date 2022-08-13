3843c58040a7db87e4f7613e67a76ea5b677415a
Jose Gutierrez <78231620+josedguti@users.noreply.github.com>
2022-08-13 16:39:30 +0200

    added creating of project per user

--- prisma/migrations/20220813143708_live_migration/migration.sql
+++ prisma/migrations/20220813143708_live_migration/migration.sql
@@ -58,6 +58,7 @@ CREATE TABLE `Project` (
     `id` VARCHAR(191) NOT NULL,
     `name` VARCHAR(191) NULL,
     `description` VARCHAR(191) NULL,
+    `userId` VARCHAR(191) NOT NULL,
 
     PRIMARY KEY (`id`)
 ) DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
